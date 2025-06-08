c639d838bb1a8f53a8eebbcc23c0bf60ff7d863b
Michael Fraser <mj.fraser99@icloud.com>
2025-06-08 12:26:36 +1000

    upgraded linter in precommit config

--- jwe/jwe.go
+++ jwe/jwe.go
@@ -9,32 +9,32 @@ package jwe
 */
 //todo: unfinished - do not use
 
-// VerifyCompactSerialization - unfinished - do not use
+////VerifyCompactSerialization - unfinished - do not use
 //func VerifyCompactSerialization(compactSerialization string, outOfBoundsPublicKey model.Retriever, opts *model.JoseOptions) (protectedHeader, body map[string]any, err error) {
 //	components := strings.Split(compactSerialization, ".")
 //	if len(components) != 5 {
-//		return nil, nil, fmt.Errorf("%winvalid compact serialization format", joseerror.MalformedToken)
+//		return nil, nil, fmt.Errorf("%winvalid compact serialization format", joseerror.ErrMalformedToken)
 //	}
 //
 //	protectedHeader, err = jsonutils.DecodeBase64urlMap(components[0])
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror decoding protected header: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror decoding protected header: %v", joseerror.ErrMalformedToken, err)
 //	}
-//	_, err = base64.RawURLEncoding.DecodeString(components[1])
+//	encryptedKey, err := base64.RawURLEncoding.DecodeString(components[1])
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror decoding encrypted key: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror decoding encrypted key: %v", joseerror.ErrMalformedToken, err)
 //	}
-//	_, err = base64.RawURLEncoding.DecodeString(components[2])
+//	initializationVector, err := base64.RawURLEncoding.DecodeString(components[2])
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror decoding initialization vector: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror decoding initialization vector: %v", joseerror.ErrMalformedToken, err)
 //	}
-//	_, err = base64.RawURLEncoding.DecodeString(components[3])
+//	cipherText, err := base64.RawURLEncoding.DecodeString(components[3])
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror decoding ciphertext: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror decoding ciphertext: %v", joseerror.ErrMalformedToken, err)
 //	}
-//	_, err = base64.RawURLEncoding.DecodeString(components[4])
+//	authenticationTag, err := base64.RawURLEncoding.DecodeString(components[4])
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror decoding authentication tag: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror decoding authentication tag: %v", joseerror.ErrMalformedToken, err)
 //	}
 //
 //	var jwkRetrievers []model.Retriever
@@ -45,20 +45,20 @@ package jwe
 //
 //	headerJwkRetrievers, err := header.ValidateHeader(protectedHeader, nil, model.JWE) //todo: sort out client providing
 //	if err != nil {
-//		return nil, nil, fmt.Errorf("%werror validating header: %v", joseerror.MalformedToken, err)
+//		return nil, nil, fmt.Errorf("%werror validating header: %v", joseerror.ErrMalformedToken, err)
 //	}
 //
 //	jwkRetrievers = append(jwkRetrievers, headerJwkRetrievers...)
 //
 //	if len(jwkRetrievers) == 0 {
-//		return nil, nil, fmt.Errorf("%wno cyptographic material provided for decryption", joseerror.MalformedToken)
+//		return nil, nil, fmt.Errorf("%wno cyptographic material provided for decryption", joseerror.ErrMalformedToken)
 //	}
 //
 //	algorithm := jwa.GetAlgorithm(protectedHeader["alg"].(string)) //validated to exist and be of correct type earlier
 //
 //	if opts != nil && len(opts.AllowedSigningAlgorithms) > 0 { //todo: this doesn't make sense for jwe
 //		if !slices.Contains(opts.AllowedSigningAlgorithms, algorithm) {
-//			return nil, nil, fmt.Errorf("%walgorithm not permitted by application-level constraints", joseerror.DecryptionFailed)
+//			return nil, nil, fmt.Errorf("%walgorithm not permitted by application-level constraints", joseerror.ErrDecryptionFailed)
 //		}
 //	}
 //
@@ -67,7 +67,7 @@ package jwe
 //		var publicKeys []crypto.PublicKey
 //		publicKeys, err = retriever()
 //		if err != nil {
-//			if !errors.Is(err, joseerror.NoKeyIdentifierMatch) {
+//			if !errors.Is(err, joseerror.ErrNoKeyIdentifierMatch) {
 //				return nil, nil, err
 //			} else {
 //				//if error is 'no key identifier', we ignore failure and store for later reporting
@@ -94,8 +94,8 @@ package jwe
 //	}
 //
 //	if len(noKeyIdentifierErrors) > 0 {
-//		return nil, nil, fmt.Errorf("%wunable to validate jws signature - possibly due to the following errors encountered retrieving cryptographic material: %v", joseerror.DecryptionFailed, noKeyIdentifierErrors)
+//		return nil, nil, fmt.Errorf("%wunable to validate jws signature - possibly due to the following errors encountered retrieving cryptographic material: %v", joseerror.ErrDecryptionFailed, noKeyIdentifierErrors)
 //	}
 //
-//	return nil, nil, fmt.Errorf("%wunable to decrypt jwe", joseerror.DecryptionFailed)
+//	return nil, nil, fmt.Errorf("%wunable to decrypt jwe", joseerror.ErrDecryptionFailed)
 //}
